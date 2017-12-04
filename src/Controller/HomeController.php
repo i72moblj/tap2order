fdea406d0625f84a3cf69cf763a38c5491276f5f
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2017-12-04 20:40:58 +0100

    Ejemplo de uso del Handler por el HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Command\GetAllCategoriesQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,16 @@ class HomeController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function index()
     {
-        return $this->render('home/index.html.twig');
+        $categories = $this->get('tactician.commandbus')->handle(
+            new GetAllCategoriesQuery()
+        );
+
+        dump($categories);
+
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 }
