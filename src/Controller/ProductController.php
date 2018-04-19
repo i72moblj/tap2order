1bae44d4e5667429c0c63ae1becc773ea39d9ce5
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-04-19 18:45:13 +0200

    Se muestra la información detallada del producto y el formulario correspondiente. Está incompleto.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,11 +4,16 @@ namespace App\Controller;
 
 
 use App\Entity\Category;
+use App\Entity\Item;
+use App\Entity\Order;
 use App\Entity\Product;
 use App\Entity\Subcategory;
+use App\Form\AddItemType;
+use App\Form\DTO\AddItem;
+use App\Services\GetTagOpenOrder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends Controller
@@ -19,9 +24,28 @@ class ProductController extends Controller
      * @ParamConverter("subcategory", options={"mapping" : {"subcategorySlug": "slug"}})
      * @ParamConverter("product", options={"mapping" : {"productSlug": "slug"}})
      */
-    public function show(Category $category, Subcategory $subcategory, Product $product)
+    public function show(Request $request, Category $category, Subcategory $subcategory, Product $product)
     {
+        $item = new Item();
+        $item->setProduct($product);
+        $order =  $this->get(GetTagOpenOrder::class)->getOrder($this->getUser());
+        $item->setOrder($order);
+        $item->setQuantity(0);
+        $item->setStatus(Order::ACTIVE);
+
+        $addItem = new AddItem();
+        $addItem->setProduct($product);
+
+        $form = $this->createForm(AddItemType::class, $addItem);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('frontend/product/view.html.twig', [
+            'form' => $form->createView(),
             'product' => $product,
         ]);
     }
