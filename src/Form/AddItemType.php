62b17bd4a8ea0b86719f8c5c0cf3461b385f1721
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-04-19 18:36:41 +0200

    Se crea clase DTO y su formulario asociado a dicha clase.
    Los formularios se pueden crear en base a una entidad, pero también se
    pueden crear en base a una clase DTO (Data Transfer Objects). Este
    último caso es cuando tenemos que hacer un formulario que muestre campos
    que no existen directamente en la entidad y que necesitamos para poblar
    otra entidad.
    
    Este es el caso de la entidad Item, necesitamos acceder a los Choices
    que corresponden a ese Producto y que luego rellenaremos ItemChoices.

---
+++ src/Form/AddItemType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Choice;
+use App\Entity\Product;
+use App\Form\DTO\AddItem;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddItemType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $data = $builder->getData();
+        /** @var Product $product */
+        $product = $data->getProduct();
+
+        $builder
+            ->add('quantity', NumberType::class, [
+                'label' => 'Cantidad',
+            ])
+            ->add('choices', EntityType::class, [
+                'class' => Choice::class,
+                'multiple' => true,
+                'query_builder' => function (EntityRepository $repository) use ($product) {
+                    return $repository
+                        ->createQueryBuilder('c')
+                        ->leftJoin('c.products', 'p')
+                        ->where('p.id = :product')
+                        ->orderBy('c.name', 'ASC')
+                        ->setParameter('product', $product->getId())
+                    ;
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => AddItem::class,
+        ]);
+    }
+}
---
+++ src/Form/DTO/AddItem.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jesules
+ * Date: 19/04/18
+ * Time: 13:34
+ */
+
+namespace App\Form\DTO;
+
+
+use App\Entity\Choice;
+use App\Entity\Product;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AddItem
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    /**
+     * @var int
+     * @Assert\GreaterThan(value="0", message="Seleccione mínimo 1")
+     */
+    private $quantity;
+
+    /**
+     * @var Collection
+     */
+    private $choices;
+
+    public function __construct()
+    {
+        $this->choices = new ArrayCollection();
+        $this->quantity = 0;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param Product $product
+     * @return AddItem
+     */
+    public function setProduct(Product $product): AddItem
+    {
+        $this->product = $product;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int $quantity
+     * @return AddItem
+     */
+    public function setQuantity(int $quantity): AddItem
+    {
+        $this->quantity = $quantity;
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getChoices(): Collection
+    {
+        return $this->choices;
+    }
+
+
+    public function addChoice(Choice $choice)
+    {
+        $this->choices[] = $choice;
+
+        return $this;
+    }
+
+    public function removeChoice(Choice $choice)
+    {
+        $this->choices->removeElement($choice);
+    }
+
+}
