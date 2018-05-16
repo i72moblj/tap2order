9832326f7a8dc91f918e5baac91c0fd03475fc83
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-05-16 23:17:56 +0200

    Se renombra servicio para que sea más descriptivo

---
+++ src/Services/GetTagOpenOrderService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Entity\Order;
+use App\Entity\Tag;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class getTagOpenOrder
+ * @package App\Service
+ */
+class GetTagOpenOrderService
+{
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    /**
+     * getTagOpenOrder constructor.
+     * @param ObjectManager $manager
+     */
+    public function __construct(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param Tag $tag
+     * @return Order
+     */
+    public function getOrder(Tag $tag): Order
+    {
+        $repository = $this->manager->getRepository(Order::class);
+        $order = $repository->findOneBy([
+            'status' => Order::ACTIVE
+        ]);
+
+        if ($order === null) {
+            $order = new Order();
+            $order->setCreatedAt(new \DateTime());
+            $order->setTotal(0);
+            $order->setTag($tag);
+            $this->manager->persist($order);
+            $this->manager->flush();
+        }
+
+        return $order;
+    }
+}
