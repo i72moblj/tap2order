01606989affaa35ecba8b874881d14964071d56f
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-06-06 01:22:11 +0200

    Se refactoriza el código. Se elimina código innecesario. Ahora el precio total de la comanda se toma de la propia comanda, antes se calculaba en el controlador. A la vista sólo se le manda la comanda, y desde ahí obtengo todo lo que necesito en la vista.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 
 use App\Command\GetAllItemsByOrderQuery;
-use App\Entity\Item;
+use App\Command\GetAllOpenOrdersQuery;
 use App\Services\GetTagOpenOrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,20 +18,12 @@ class OrderController extends Controller
 
         $order = $openOrder->getOrder($this->getUser());
 
-        $items = $this->get('tactician.commandbus')->handle(
+        $this->get('tactician.commandbus')->handle(
             new GetAllItemsByOrderQuery($order->getId())
         );
 
-        $total = 0;
-        foreach ($items as $item) {
-            /** @var Item $item */
-            $total = $total + ($item->getPrice() * $item->getQuantity());
-        }
-
         return $this->render('frontend/order/show.html.twig', [
-            'order' => $order,
-            'items' => $items,
-            'total' => $total,
+            'order' => $order
         ]);
     }
 }
