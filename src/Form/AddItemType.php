61c52700a8ec15dc6a95631e672ce87ee8faa537
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-10-09 23:12:10 +0200

    Se añade atributo choice_label, pero no muestra lo que debe. Se cambia el orden de los elementos del formulario.

--- src/Form/AddItemType.php
+++ src/Form/AddItemType.php
@@ -8,6 +8,7 @@ use App\Form\DTO\AddItem;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,14 +22,19 @@ class AddItemType extends AbstractType
         $product = $data->getProduct();
 
         $builder
-            ->add('quantity', NumberType::class, [
-                'label' => 'Cantidad:',
-            ])
             ->add('choices', EntityType::class, [
                 'label' => 'Elección:',
                 'class' => Choice::class,
                 'multiple' => true,
                 'expanded' => true,
+                'choice_label' => function ($choice) {
+                    $numero = $choice->getAllergens()->count();
+                    $allergens = '';
+                    foreach ($choice->getAllergens() as $allergen) {
+                        $allergens = $allergens . ', ' . $allergen;
+                    }
+                    return $choice->getId() . ' ' . $choice->getName() . $numero . ' Alérgenos: ' . trim($allergens,',' . ' +' . $choice->getPrice());
+                },
                 'query_builder' => function (EntityRepository $repository) use ($product) {
                     return $repository
                         ->createQueryBuilder('c')
@@ -38,7 +44,22 @@ class AddItemType extends AbstractType
                         ->setParameter('product', $product->getId())
                         ->setParameter('isEnabled', true)
                     ;
-                }
+                },
+            ])
+            ->add('quantity', ChoiceType::class, [
+                'label' => 'Cantidad:',
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    '10' => 10,
+                ]
             ])
         ;
     }
