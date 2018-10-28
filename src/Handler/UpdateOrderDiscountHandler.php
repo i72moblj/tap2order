bb7670aef813d19d21293a8794bd33826c250f44
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-10-28 12:31:02 +0100

    Se calculan los descuentos según tipo de oferta.
    Ojo, no se han calculado incluyendo las variedades del artículo.

--- src/Handler/UpdateOrderDiscountHandler.php
+++ src/Handler/UpdateOrderDiscountHandler.php
@@ -38,18 +38,74 @@ class UpdateOrderDiscountHandler
             $order->setDiscount(0);
         }
         else {
+            $offerType = $order->getOffer()->getType();
+
+            // Calcular el número de artículos que son del producto base
+            $items = $order->getItems();
+
+            $orderBaseProductQuantity = 0;
+            foreach ($items as $item)
+            {
+                if ($item->getProduct() === $offer->getBaseProduct()) {
+                    $orderBaseProductQuantity = $orderBaseProductQuantity + $item->getQuantity();
+                }
+            }
+
+            // TIPO = DESCUENTO
+            if ($offerType === Offer::DISCOUNT) {
+
+                // MODO = NUEVO PRECIO
+                if ($offer->getMode() === Offer::NEW_PRICE) {
+                    $discount = ($offer->getBaseProduct()->getPrice() - $offer->getValue()) * $orderBaseProductQuantity;
+                }
+                // MODO = PORCENTAJE DE DESCUENTO
+                else {
+                    $discount = ($offer->getBaseProduct()->getPrice() * $offer->getValue() / 100) * $orderBaseProductQuantity;
+
+                }
+            }
+            // TIPO = DESCUENTO COMBINADO
+            else {
+                // Producto base = Producto combinado
+                if ($offer->getBaseProduct() === $offer->getCombinedProduct()) {
+                    $offersQuantity = intdiv($orderBaseProductQuantity, $offer->getBaseProductQuantity());
+
+                    $discount = ($offer->getBaseProduct()->getPrice() * $offer->getBaseProductQuantity()) - $offer->getValue();
+                }
+                // Producto base != Producto combinado
+                else {
+                    // Calcular el número de ofertas que se cumplen
+                    $orderCombinedProductQuantity = 0;
+                    foreach ($items as $item)
+                    {
+                        if ($item->getProduct() === $offer->getCombinedProduct()) {
+                            $orderCombinedProductQuantity = $orderCombinedProductQuantity + $item->getQuantity();
+                        }
+                    }
+
+                    $offersQuantity = min($orderBaseProductQuantity, $orderCombinedProductQuantity);
+
+                    // MODO = NUEVO PRECIO
+                    if ($offer->getMode() === Offer::NEW_PRICE) {
+                        $discount = ($offer->getBaseProduct()->getPrice() + $offer->getCombinedProduct()->getPrice() - $offer->getValue()) * $offersQuantity;
+                    }
+                    // MODO = PORCENTAJE DE DESCUENTO
+                    else {
+                        $discount = ($offer->getBaseProduct()->getPrice() + $offer->getCombinedProduct()->getPrice()) * $offer->getValue() / 100 * $offersQuantity;
+                    }
+                }
+            }
 
 
 
 
 
 
-            // AQUI ES DONDE EL MEOLLO DE LAS OFERTAS
-//            $discount = ...
-//
-//
-//
-//            $order->setDiscount($discount);
+
+
+
+
+            $order->setDiscount($discount);
         }
 
         $this->manager->flush();
