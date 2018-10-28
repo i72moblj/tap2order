83efc96e80dd0772d5b8427d250d67bac4ead6c0
Jesús Manuel Moreno Blanco <i72moblj@uco.es>
2018-10-29 00:02:38 +0100

    Se crea la vista y el método para cambiar el estado del artículo de preparado a servido

--- src/Controller/Waiters/OrderController.php
+++ src/Controller/Waiters/OrderController.php
@@ -10,6 +10,8 @@ namespace App\Controller\Waiters;
 
 
 use App\Command\GetAllOpenOrdersQuery;
+use App\Command\UpdateItemStatusCommand;
+use App\Entity\Item;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,13 +21,34 @@ class OrderController extends Controller
      * @Route("/waiters", name="waiters_index")
      */
     public function index() {
-        $openOrders = $this->get('tactician.commandbus')->handle(
+        $orders = $this->get('tactician.commandbus')->handle(
             new GetAllOpenOrdersQuery()
         );
 
         return $this->render('waiters/order/waiters_index.html.twig', [
-            'openOrders' => $openOrders,
+            'orders' => $orders,
         ]);
+    }
+
+    /**
+     * @Route("/waiters/{item}/served", name="item_served")
+     *
+     * @param Item $item
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function setServed(Item $item)
+    {
+        /** @var Item $item */
+        $item->setStatus(Item::SERVED);
+
+        $this->get('tactician.commandbus')->handle(
+            new UpdateItemStatusCommand(
+                $item,
+                Item::SERVED
+            )
+        );
+
+        return $this->redirectToRoute('waiters_index');
     }
 
 }
